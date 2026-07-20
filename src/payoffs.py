b51f46e5ca9bb20a2914f9cf52c3be8449a5a3d6
avinashs0722 <avinash.s0722@outlook.com>
2026-07-20 13:08:32 +0100

    Delete src directory

--- src/payoffs.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def call_payoff(stock_prices, strike):
-    return np.maximum(stock_prices - strike, 0)
-
-
-def put_payoff(stock_prices, strike):
-    return np.maximum(strike - stock_prices, 0)
